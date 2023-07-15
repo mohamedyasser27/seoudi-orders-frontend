9ce7d12499df0dea224b4556d70bdae0658021e3
mohamedyasser27 <my2722000@gmail.com>
2023-07-15 12:54:13 +0300

    Add Transition when showing Tables

--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -21,61 +21,77 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div
-    class="max-w-5xl rounded-lg bg-gray-100 p-0 shadow-lg lg:p-3"
-    v-if="allOrders.length != 0"
-  >
-    <table class="w-full table-auto" aria-label="Orders Table">
-      <thead>
-        <tr>
-          <th
-            v-for="columnName in columnNames"
-            :class="`${
-              currentSort == columnName && 'text-red-700'
-            }  border-b border-gray-300 bg-gray-200 px-1 py-2 text-left text-[.6rem] md:text-[.8rem] lg:px-4 lg:text-base`"
-          >
-            <button
-              class="flex flex-wrap items-center justify-center gap-1"
-              @click="
-                () => {
-                  fetchOrdersData(columnName).then((data) => {
-                    currentSort = columnName;
-                    allOrders = data;
-                  });
-                }
-              "
+  <Transition name="fade" mode="out-in">
+    <div
+      class="max-w-5xl rounded-lg bg-gray-100 p-0 shadow-lg lg:p-3"
+      v-if="allOrders.length != 0"
+    >
+      <table class="w-full table-auto" aria-label="Orders Table">
+        <thead>
+          <tr>
+            <th
+              v-for="columnName in columnNames"
+              :class="`${
+                currentSort == columnName && 'text-red-700'
+              }  border-b border-gray-300 bg-gray-200 px-1 py-2 text-left text-[.6rem] md:text-[.8rem] lg:px-4 lg:text-base`"
             >
-              {{ columnName.split("_").join(" ") }}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                class="h-[15px] w-[15px] fill-current lg:h-[24px] lg:w-[24px]"
+              <button
+                class="flex flex-wrap items-center justify-center gap-1"
+                @click="
+                  () => {
+                    fetchOrdersData(columnName).then((data) => {
+                      currentSort = columnName;
+                      allOrders = data;
+                    });
+                  }
+                "
               >
-                <path d="M0 0h24v24H0V0z" fill="none" />
-                <path d="M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z" />
-              </svg>
-            </button>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          class="transition-colors duration-100 hover:bg-slate-500 hover:text-slate-50"
-          v-for="order in allOrders"
-        >
-          <td
-            v-for="(columnValue, columnName) in order"
-            class="px-1 py-2 text-[.7rem] md:text-[.6rem] lg:px-4 lg:text-base"
+                {{ columnName.split("_").join(" ") }}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  class="h-[15px] w-[15px] fill-current lg:h-[24px] lg:w-[24px]"
+                >
+                  <path d="M0 0h24v24H0V0z" fill="none" />
+                  <path d="M10 18h4v-2h-4v2zM3 6v2h18V6H3zm3 7h12v-2H6v2z" />
+                </svg>
+              </button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            class="transition-colors duration-100 hover:bg-slate-500 hover:text-slate-50"
+            v-for="order in allOrders"
           >
-            {{
-              columnName === "order_date"
-                ? getLocaleDateString(columnValue)
-                : columnValue
-            }}
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-  <NoDataFound :error-msg="'No Data Found'" class="" v-else></NoDataFound>
+            <td
+              v-for="(columnValue, columnName) in order"
+              class="px-1 py-2 text-[.7rem] md:text-[.6rem] lg:px-4 lg:text-base"
+            >
+              {{
+                columnName === "order_date"
+                  ? getLocaleDateString(columnValue)
+                  : columnValue
+              }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <NoDataFound :error-msg="'No Data Found'" class="" v-else></NoDataFound>
+  </Transition>
 </template>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition:
+    opacity 0.2s ease-in-out,
+    translate 0.2s ease-in-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  translate: 0px 20px;
+}
+</style>
--- src/views/StatisticsView.vue
+++ src/views/StatisticsView.vue
@@ -15,34 +15,50 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div
-    v-if="statistics.length != 0"
-    class="max-w-5xl rounded-lg bg-gray-100 p-4 shadow-lg"
-  >
+  <Transition name="fade" mode="out-in">
     <div
-      class="grid grid-cols-1 items-center justify-center gap-4 md:grid-cols-2"
+      v-if="statistics.length != 0"
+      class="max-w-5xl rounded-lg bg-gray-100 p-4 shadow-lg"
     >
       <div
-        v-for="item in statistics"
-        :key="item.name"
-        class="group h-full rounded-lg bg-gray-200 p-8 text-center transition-colors duration-100 hover:bg-slate-500"
-        style="width: 300px"
+        class="grid grid-cols-1 items-center justify-center gap-4 md:grid-cols-2"
       >
-        <h3
-          class="mb-4 text-2xl font-bold text-blue-600 transition-colors duration-100 group-hover:text-blue-300"
+        <div
+          v-for="item in statistics"
+          :key="item.name"
+          class="group h-full rounded-lg bg-gray-200 p-8 text-center transition-colors duration-100 hover:bg-slate-500"
+          style="width: 300px"
         >
-          {{ item.name }}
-        </h3>
-        <p
-          class="text-4xl transition-colors duration-100 group-hover:text-slate-50"
-        >
-          {{ item.value }}
-        </p>
+          <h3
+            class="mb-4 text-2xl font-bold text-blue-600 transition-colors duration-100 group-hover:text-blue-300"
+          >
+            {{ item.name }}
+          </h3>
+          <p
+            class="text-4xl transition-colors duration-100 group-hover:text-slate-50"
+          >
+            {{ item.value }}
+          </p>
+        </div>
       </div>
     </div>
-  </div>
-  <NoDataFound
-    :error-msg="'No Data Found'"
-    v-else="statistics.length == 0"
-  ></NoDataFound>
+    <NoDataFound
+      :error-msg="'No Data Found'"
+      v-else="statistics.length == 0"
+    ></NoDataFound>
+  </Transition>
 </template>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition:
+    opacity 0.2s ease-in-out,
+    translate 0.2s ease-in-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  translate: 0px 20px;
+}
+</style>
